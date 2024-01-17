1cd70b938db4e8268cec0e8e5e54404793f3d8d5
edvard01 <frejaclueless@gmail.com>
2024-01-17 10:44:57 +0100

    Setting up files and initial code for the Sauna

---
+++ js/index.js
@@ -0,0 +1,9 @@
+let temp = 0;
+
+do {
+  temp = parseInt(
+    prompt(
+      "Welcome to the Sauna! Please input a temperature in celsius to have it converted to Fahrenheit."
+    )
+  );
+} while (true);
